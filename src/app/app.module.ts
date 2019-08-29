70df17b94e4fe15e7af7fb990baaa0ef068b1e66
Andre Luis <aisitiger.98@gmail.com>
2019-08-29 13:56:32 -0300

    adicionando pino com cor customizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -27,6 +28,7 @@ import { RecuperarSenhaPageModule } from './pages/modals/recuperar-senha/recuper
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { IntroModalPage } from '../modals/intro-modal/intro-modal.page';
 
-import mapboxgl  from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-home',
@@ -13,19 +14,29 @@ import mapboxgl  from 'mapbox-gl';
 })
 export class HomePage {
 
+  public currentPosition: any;
+  private map: any;
+
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private geolocation: Geolocation
   ) {
 
   }
 
   ngOnInit() {
-    
+
   }
 
   ionViewWillEnter() {
-   this.openModalPage();
-  this.initializeMap();
+    this.openModalPage();
+    this.initializeMap();
+  }
+
+  ionViewDidEnter() {
+    this.getAtualPosition().then(() => {
+      this.setPosition();
+    });
   }
 
   private async openModalPage() {
@@ -38,9 +49,26 @@ export class HomePage {
 
   private initializeMap(): void {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGlvbmltIiwiYSI6ImNqejA0Mm54OTA0MHkzb3Fpemo5cnhmYWcifQ.gbYcjV1OcISZp1Ym1xw8pw';
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/dionim/cjzwtgft014k41csdy9xmjcyq'
+      style: 'mapbox://styles/dionim/cjzwtgft014k41csdy9xmjcyq',
+      zoom: 12
     });
+  }
+
+  private async getAtualPosition() {
+    await this.geolocation.getCurrentPosition().then((resp) => {
+      this.currentPosition = resp.coords;
+    })
+  }
+
+  private setPosition() {
+    this.map.setCenter([this.currentPosition.longitude, this.currentPosition.latitude]);
+    this.markerCurrentPosition();
+  }
+
+  private markerCurrentPosition() {
+    let marker = new mapboxgl.Marker({color: '#D6A763'}).setLngLat([this.currentPosition.longitude, this.currentPosition.latitude]).addTo(this.map);
+    console.log(marker);
   }
 }
