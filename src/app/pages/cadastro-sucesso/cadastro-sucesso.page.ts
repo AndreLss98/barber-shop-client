891d8421ed704c247d3e897d1ee41c89070af2a2
Andre Luis <andre_luis98@outlook.com>
2019-12-19 08:58:54 -0300

    formantando classe de cadastro com sucesso

--- src/app/pages/cadastro-sucesso/cadastro-sucesso.page.ts
+++ src/app/pages/cadastro-sucesso/cadastro-sucesso.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-sucesso',
@@ -13,7 +13,7 @@ export class CadastroSucessoPage implements OnInit {
   }
 
   ngOnInit() {
-
+    
   }
 
   public goToHome() {
